82f2848abce7722874078b3cce34aaa5b1ba71bb
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-05-21 10:24:34 -0300

    Implementando logica en el Header de la pagina

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Navbar,Nav} from 'react-bootstrap';
 
 const Header = () => (
-        <div>
-            Header
-        </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="/">Post</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/create">Crear un post</Nav.Link>
+      </Nav>
+      
+    </Navbar.Collapse>
+  </Navbar>
     )
 
 export default Header
