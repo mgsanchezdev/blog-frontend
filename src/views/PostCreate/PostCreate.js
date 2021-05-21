ab77c14b7b3d8592b5b5ba57dbe51e1a239b475f
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-05-21 10:43:34 -0300

    Implementacion de la logica para crear un post,POST Simula la creación de un nuevo post

--- src/views/PostCreate/PostCreate.js
+++ src/views/PostCreate/PostCreate.js
@@ -1,9 +1,71 @@
-import React from 'react'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 
-const PostCreate = () => (
-        <div>
-            Formulario para crear el post
+import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { Button } from 'react-bootstrap';
+import http from '../../utils/http';
+import { messageAlert } from '../../utils/alerts';
+import './postCreate.css';
+
+const PostCreate = () => {
+  const history = useHistory();
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (formData) => {
+    const formDataCopy = { ...formData };
+
+    if (formDataCopy.title !== '' && formDataCopy.body) {
+      http.post(`posts`, formDataCopy).then((result) => {
+        messageAlert('success', 'Post creado', result.data.title).then(() => {
+          history.push('/');
+        });
+      });
+    } else {
+      messageAlert(
+        'error',
+        'Error al crear el Post',
+        'Complete todos los campos'
+      );
+    }
+  };
+
+  return (
+    <div className="edit-form">
+      <h1 className="title-edit-post">Crear el post</h1>
+      <form className="edit-form form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-row">
+          <div className="form-control">
+            <label htmlFor="title">Título</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              {...register('title')}
+              placeholder="título"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="body">Post</label>
+            <textarea
+              maxLength="140"
+              name="body"
+              id="body"
+              {...register('body')}
+              placeholder="Contenido del post"
+            />
+          </div>
         </div>
-    )
 
-export default PostCreate
+        <div className="submit-button-wrapper">
+          <Button type="submit" className="btn" variant="success">
+            Crear post
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default PostCreate;
